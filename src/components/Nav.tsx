c35aaa040832ee3b8c5816c6d2e2521d7d18c230
hlccare <280108904@qq.com>
2021-04-05 15:50:40 +0800

    导航栏添加icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+require('icons/money.svg')
+require('icons/label.svg')
+require('icons/statistic.svg')
+
 
 const NavWrapper = styled.nav`
 line-height: 24px;
@@ -8,8 +12,16 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   display:flex;
   >li{
     width: 33.33%;
-    padding: 16px;
+    padding: 4px 0;
     text-align:center;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    .icon{
+        width: 24px;
+        height: 2px;
+    }
   }
 }
 `
@@ -19,12 +31,21 @@ const Nav = ()=>{
         <NavWrapper>
           <ul>
             <li>
+                <svg className='icon'>
+                    <use xlinkHref='#label'></use>
+                </svg>
               <Link to="/tags">标签</Link>
             </li>
             <li>
+                <svg className='icon'>
+                    <use xlinkHref='#money'></use>
+                </svg>
               <Link to="/money">记账</Link>
             </li>
             <li>
+                <svg className='icon'>
+                    <use xlinkHref='#statistic'></use>
+                </svg>
               <Link to="/statistics">统计</Link>
             </li>
           </ul>
