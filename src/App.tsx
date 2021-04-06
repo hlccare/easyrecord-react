c2458a5b64c9ca2b9028482d644a67e2fed58085
hlccare <280108904@qq.com>
2021-04-06 11:41:30 +0800

    money的TagsSection UI  完成

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,34 @@ import Tags from "views/Tags";
 import Money from "views/Money";
 import Statistics from "views/Statistics";
 import NoMatch from "views/NoMatch";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: #333;
+`
 
 
-
-
- function App() {
+function App() {
   return (
-    <Router>
-          <Switch>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch> 
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/money" />
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
