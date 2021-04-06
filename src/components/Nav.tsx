4209a2a3fc19be3c124f982c0aa8c9121d0126e2
hlccare <280108904@qq.com>
2021-04-06 10:23:42 +0800

    修复nav点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,38 +10,40 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   display:flex;
   >li{
     width: 33.33%;
-    padding: 4px 0;
     text-align:center;
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    .icon{
-        width: 24px;
-        height: 24px;
+    > a{
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      padding: 4px 0;
+      .icon{
+          width: 24px;
+          height: 24px;
+      }
     }
   }
 }
 `
-const Nav = ()=>{
-    return(
-        
-        <NavWrapper>
-          <ul>
-            <li>
-                <Icon name='label'/>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-                <Icon name='money'/>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-                <Icon name='statistic'/>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </NavWrapper>
-        )
+const Nav = () => {
+  return (
+
+    <NavWrapper>
+      <ul>
+        <li>
+
+          <Link to="/tags"><Icon name='label' />标签</Link>
+        </li>
+        <li>
+
+          <Link to="/money"><Icon name='money' />记账</Link>
+        </li>
+        <li>
+
+          <Link to="/statistics"><Icon name='statistic' />统计</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  )
 }
 export default Nav;
